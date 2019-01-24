a3d1c53c1c56f53e8c9211a7f73c1509978d9e35
arafath <arafath.ahmed@goodworklabs.com>
2019-01-24 11:13:52 +0530

    ADDED: available coins counter to homePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,14 @@
-import { Component, Inject, NgZone } from '@angular/core';
-import { App, NavController, Platform } from 'ionic-angular';
-import { AppPreferences } from '@ionic-native/app-preferences';
-import { PreferenceKey } from '../../config/constants';
-import { UserService } from '../../services/user/user.service';
-import { StallListPage } from '../stall-list/stall-list';
-import { GetUserResponse } from '../../services/user/response';
-import { StallService } from '../../services/stall/stall-service';
-import { LoadingController } from 'ionic-angular';
+import {Component, Inject, NgZone} from '@angular/core';
+import {App, Loading, LoadingController, NavController, Platform} from 'ionic-angular';
+import {AppPreferences} from '@ionic-native/app-preferences';
+import {PreferenceKey} from '../../config/constants';
+import {UserService} from '../../services/user/user.service';
+import {StallListPage} from '../stall-list/stall-list';
+import {GetUserResponse} from '../../services/user/response';
+import {StallService} from '../../services/stall/stall-service';
+import {AppConfig} from '../../config/AppConfig';
+import {BoughtIdeas} from '../../services/stall/BoughtIdeas';
+import {Subscription} from 'rxjs';
 
 declare const cordova;
 
@@ -15,6 +17,9 @@ declare const cordova;
     templateUrl: 'home.html'
 })
 export class HomePage {
+    private boughtIdeas: BoughtIdeas = {};
+    private boughtIdeasSubscription: Subscription;
+
     public userResponse?: GetUserResponse;
     public qrCodeWidth?: number;
 
@@ -28,16 +33,19 @@ export class HomePage {
         private platform: Platform,
         private loadingCtrl: LoadingController,
         @Inject('USER_SERVICE') private userService: UserService,
-        @Inject('STALL_SERVICE') private stallService: StallService
+        @Inject('STALL_SERVICE') private stallService: StallService,
+        @Inject('APP_CONFIG') private config: AppConfig
     ) {
     }
 
     ionViewDidEnter() {
-
         this.qrCodeWidth = this.platform.width() - (this.platform.width() * 0.4);
-
         this.fetchUserDetails();
+        this.fetchBoughtIdeas();
+    }
 
+    ionViewWillLeave() {
+        this.boughtIdeasSubscription.unsubscribe();
     }
 
     navigateToStallListPage() {
@@ -56,10 +64,21 @@ export class HomePage {
 
     }
 
-    getLoader(): any {
+    public getAvailableCoins(): any {
+        const purchasedIdeas: number = Object.keys(this.boughtIdeas).length;
+        return this.config.availableCoins - (purchasedIdeas * this.config.coinsPerIdea);
+    }
+
+    private getLoader(): Loading {
         return this.loadingCtrl.create({
             duration: 30000,
             spinner: 'crescent'
+        });
+    }
+
+    private fetchBoughtIdeas() {
+        this.boughtIdeasSubscription = this.stallService.getBoughtIdeas().subscribe((emit) => {
+            this.boughtIdeas = emit;
         });
     }
 }
