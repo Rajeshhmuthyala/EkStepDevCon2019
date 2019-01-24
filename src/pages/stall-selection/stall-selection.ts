0c956c2c979241811ed07f73f8c6b766414540d7
arafath <arafath.ahmed@goodworklabs.com>
2019-01-24 11:40:25 +0530

    CLEANUP

--- src/pages/stall-selection/stall-selection.ts
+++ src/pages/stall-selection/stall-selection.ts
@@ -1,13 +1,8 @@
-import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Component, Inject} from '@angular/core';
+import {IonicPage, NavController, NavParams, ViewController} from 'ionic-angular';
+import {FormBuilder, Validators} from '@angular/forms';
 import {StallService} from '../../services/stall/stall-service';
-import {AppPreferences} from '@ionic-native/app-preferences';
-import {PreferenceKey} from '../../config/constants';
-import {GetIdeasResponse} from '../../services/stall/responses';
-import {Stall} from '../../services/stall/Stall';
-import { timestamp } from 'rxjs/operators';
-import { StallQRScanPage } from '../stall-qr-scan/stall-qr-scan.component';
+import {StallQRScanPage} from '../stall-qr-scan/stall-qr-scan.component';
 
 /**
  * Generated class for the StallSelectionPage page.
@@ -18,53 +13,56 @@ import { StallQRScanPage } from '../stall-qr-scan/stall-qr-scan.component';
 
 @IonicPage()
 @Component({
-  selector: 'page-stall-selection',
-  templateUrl: 'stall-selection.html',
+    selector: 'page-stall-selection',
+    templateUrl: 'stall-selection.html',
 })
 export class StallSelectionPage {
 
-  stallForm: any;
- ideasResponse: any;
- stallcode: any;
+    stallForm: any;
+    ideasResponse: any;
+    stallcode: any;
 
- 
-  constructor(
-    public navCtrl: NavController,
-     public navParams: NavParams,
-     private fb: FormBuilder, @Inject('STALL_SERVICE') private stallService: StallService,
-      private viewCtrl:ViewController) {
-      this.initializeForm();
-  }
-  initializeForm() {   
-    this.stallForm = this.fb.group({
-      stallName: ['', Validators.required],
-      stallIdea: ['', Validators.required]
-    });
-  }
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad StallSelectionPage');
-    this.fetchStallDetails();
-  }
 
-  private async fetchStallDetails() {
-    // this.stallForm = await this.stallService.getStalls({ Stall: {} });
-    this.stallcode =  await this.stallService.getStalls({ Stall: {} });
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        private fb: FormBuilder, @Inject('STALL_SERVICE') private stallService: StallService,
+        private viewCtrl: ViewController) {
+        this.initializeForm();
+    }
 
-    console.log(this.stallcode);
-  }
- 
-  public onStallSelect() {
-    console.log('hi', this.stallForm.value);    
-    const stallCode = this.stallForm.value.stallName;
-    console.log(stallCode);
-    this.stallService.getIdeas({code: stallCode}).then((data) => {
-      console.log(data);
-      this.ideasResponse = data.Stall.ideas;
-    });
+    initializeForm() {
+        this.stallForm = this.fb.group({
+            stallName: ['', Validators.required],
+            stallIdea: ['', Validators.required]
+        });
+    }
 
-}
-StallSubmit(){
-  this.navCtrl.push(StallQRScanPage, {});
-   this.viewCtrl.dismiss();
-}
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad StallSelectionPage');
+        this.fetchStallDetails();
+    }
+
+    public onStallSelect() {
+        console.log('hi', this.stallForm.value);
+        const stallCode = this.stallForm.value.stallName;
+        console.log(stallCode);
+        this.stallService.getIdeas({code: stallCode}).then((data) => {
+            console.log(data);
+            this.ideasResponse = data.Stall.ideas;
+        });
+
+    }
+
+    StallSubmit() {
+        this.navCtrl.push(StallQRScanPage, {});
+        this.viewCtrl.dismiss();
+    }
+
+    private async fetchStallDetails() {
+        // this.stallForm = await this.stallService.getStalls({ Stall: {} });
+        this.stallcode = await this.stallService.getStalls({Stall: {}});
+
+        console.log(this.stallcode);
+    }
 }
